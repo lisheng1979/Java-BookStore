30729b862e7f8d806bb69b1e6969f20ec83598f6
lisheng1979 <skyeyes@263.net>
2020-04-26 23:15:02 -0700

    Completed module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -67,6 +67,9 @@ public class ControllerServlet extends HttpServlet {
 			case "/insert":
 				insertBook(request, response);
 				break;
+			case "/update":
+				updateBook(request, response);
+				break;
 			default:
 				listBooks(request, response);
 				break;
@@ -92,7 +95,18 @@ public class ControllerServlet extends HttpServlet {
 		request.setAttribute("book", book);
 		requestDispatcher.forward(request, response);
 	}
-
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		float price = Float.parseFloat(request.getParameter("bookprice"));
+		Book book = new Book(id, title, author, price);
+		bookDAO.updateBook(book);
+		response.sendRedirect("list");
+	}
+	
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
 			throws ClassNotFoundException, SQLException, ServletException, IOException {
 		ArrayList<Book> books_list = bookDAO.listAllBooks();
